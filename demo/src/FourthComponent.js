88253969e3b327f27548d4cfffb17602dc2adea2
Viktor <victor.liegostaiev@gmail.com>
2017-11-19 01:40:24 +0100

    major change ( goToPage method) v. 1.1.0

--- demo/src/FourthComponent.js
+++ demo/src/FourthComponent.js
@@ -1,12 +1,6 @@
 import React from "react";
 
-export default ({goToPage}) => {
-
-    const selectPage = (number) => {
-        let pageNumber = +prompt("Enter page number(1 - 5)");
-        goToPage(pageNumber);
-    };
-
+export default () => {
     return (
         <div className="component fourth-component">
             <h2 style={{margin: "auto"}}>Fourth Component</h2>
