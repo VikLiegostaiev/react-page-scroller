c12c906210fc3809d1feb79bd939da2a68e21257
Sergei Morozov <morozserge1st@gmail.com>
2020-07-20 14:39:21 +0300

    feat: add ts types

---
+++ src/index.d.ts
@@ -0,0 +1,64 @@
+// Type definitions for react-page-scroller 2.0
+// Project: https://vikliegostaiev.github.io/react-page-scroller/
+// Definitions by: Sergei Morozov <https://github.com/morozserge1st>
+
+import * as React from "react";
+
+export interface ReactPageScrollerProps {
+  /**
+   * Animation duration in milliseconds.
+   */
+  animationTimer?: number;
+  /**
+   * Animation buffer timing in milliseconds.
+   */
+  animationTimerBuffer?: number;
+  /**
+   * If `true`, scrolling down will be blocked.
+   */
+  blockScrollDown?: boolean;
+  /**
+   * If `true`, scrolling up will be blocked.
+   */
+  blockScrollUp?: boolean;
+  /**
+   * The children of the component.
+   */
+  children?: React.ReactNode;
+  /**
+   * The height of react-page-scroller child.
+   */
+  containerHeight?: number | string;
+  /**
+   * The width of react-page-scroller child.
+   */
+  containerWidth?: number | string;
+  /**
+   * The index of the selected page.
+   *
+   * The index of pages should start with 0.
+   */
+  customPageNumber?: number;
+  /**
+   * Callback fired when someone tries to scroll over last or first child component.
+   */
+  handleScrollUnavailable?: () => void;
+  /**
+   * Callback fired when the selected page changes.
+   *
+   * @param {number} page The number of next page.
+   */
+  pageOnChange?: (page: number) => void;
+  /**
+   * 	If `true`, all pages will be rendered at the first rendering component.
+   */
+  renderAllPagesOnFirstRender?: boolean;
+  /**
+   * CSS transition timing function name.
+   */
+  transitionTimingFunction?: string;
+}
+
+export default function ReactPageScroller(
+  props: ReactPageScrollerProps,
+): JSX.Element;
