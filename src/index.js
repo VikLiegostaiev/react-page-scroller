204410006dbbe9df9e5ef1c5dad78d46cf16b636
LeoneBacciu <leonebacciu@gmail.com>
2021-02-17 21:33:09 +0100

    Update code styling (prettier)

--- src/index.js
+++ src/index.js
@@ -30,20 +30,20 @@ let isTransitionAfterComponentsToRenderChanged = false;
 const containers = [];
 
 const ReactPageScroller = ({
-                             animationTimer,
-                             animationTimerBuffer,
-                             blockScrollDown,
-                             blockScrollUp,
-                             children,
-                             containerHeight,
-                             containerWidth,
-                             customPageNumber,
-                             handleScrollUnavailable,
-                             onBeforePageScroll,
-                             pageOnChange,
-                             renderAllPagesOnFirstRender,
-                             transitionTimingFunction,
-                           }) => {
+  animationTimer,
+  animationTimerBuffer,
+  blockScrollDown,
+  blockScrollUp,
+  children,
+  containerHeight,
+  containerWidth,
+  customPageNumber,
+  handleScrollUnavailable,
+  onBeforePageScroll,
+  pageOnChange,
+  renderAllPagesOnFirstRender,
+  transitionTimingFunction,
+}) => {
   const [componentIndex, setComponentIndex] = useState(DEFAULT_COMPONENT_INDEX);
   const [componentsToRenderLength, setComponentsToRenderLength] = useState(
     DEFAULT_COMPONENTS_TO_RENDER_LENGTH,
@@ -60,7 +60,7 @@ const ReactPageScroller = ({
       }
 
       pageContainer.current.style.transform = `translate3d(0, ${nextComponentIndex *
-      -100}%, 0)`;
+        -100}%, 0)`;
     },
     [onBeforePageScroll],
   );
